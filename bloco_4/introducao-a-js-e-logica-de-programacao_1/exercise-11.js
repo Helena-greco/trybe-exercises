397703bc1c20f567c648de94ed4e63ba3bf5550a
Helena <nenafgreco@hotmail.com>
2021-06-23 22:57:38 -0300

    exercicio 10

---
+++ bloco_4/introducao-a-js-e-logica-de-programacao_1/exercise-11.js
@@ -0,0 +1,11 @@
+
+let costOfProduct = 1;
+let saleValue = 3;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+  let totalCostOfProduct = costOfProduct * 1.2;
+  let totalProfit = (saleValue - totalCostOfProduct) * 1000;
+  console.log(totalProfit);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
