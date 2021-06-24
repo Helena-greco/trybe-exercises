f28a9f64172a6981e89db858ffa585ff512442a5
Helena <nenafgreco@hotmail.com>
2021-06-23 21:54:29 -0300

    Resolução dos exercícios 1, 2 e 3

---
+++ bloco_4/introducao-a-js-e-logica-de-programacao_1/exercise-1.js
@@ -0,0 +1,9 @@
+
+let a = 10;
+let b = 5;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação: ' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Módulo: ' + (a % b));
---
+++ bloco_4/introducao-a-js-e-logica-de-programacao_1/exercise-2.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+
+if (a > b) {
+  console.log("a é maior que b")
+} else {
+  "b é maior que a"
+}
