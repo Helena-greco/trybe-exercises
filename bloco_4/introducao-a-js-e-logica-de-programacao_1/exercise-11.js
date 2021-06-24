d09cafb11c0cf95eef6434504c2984c566447afa
Helena <nenafgreco@hotmail.com>
2021-06-23 23:15:23 -0300

    exercicio 11

--- bloco_4/introducao-a-js-e-logica-de-programacao_1/exercise-11.js
+++ bloco_4/introducao-a-js-e-logica-de-programacao_1/exercise-11.js
@@ -1,11 +1,32 @@
+let INSS;
+let IR;
 
-let costOfProduct = 1;
-let saleValue = 3;
+let salarioBruto = 3000;
 
-if (costOfProduct >= 0 && saleValue >= 0) {
-  let totalCostOfProduct = costOfProduct * 1.2;
-  let totalProfit = (saleValue - totalCostOfProduct) * 1000;
-  console.log(totalProfit);
+if (salarioBruto <= 1556.94) {
+  INSS = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92) {
+  INSS = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82) {
+  INSS = salarioBruto * 0.11;
 } else {
-  console.log("Error, os valores não podem ser negativos");
-};
+  INSS = 570.88;
+}
+
+let salarioBase = salarioBruto - INSS;
+
+if (salarioBase <= 1903.98) {
+  IR = 0
+} else if (salarioBase <= 2826.65) {
+  IR = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase <= 3751.05) {
+  IR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68) {
+  IR = (salarioBase * 0.225) - 636.13;
+} else {
+  IR = (salarioBase * 0.275) - 869,36
+}
+
+let salarioLiquido = salarioBase - IR;
+
+console.log(salarioLiquido);
