56abc63141507acd006e28c8fe45054127e97aa1
Helena <nenafgreco@hotmail.com>
2021-06-29 09:50:52 -0300

    Exercicio 1 de função

--- bloco_4/fundamentos/function.js
+++ bloco_4/fundamentos/function.js
@@ -1,12 +1,12 @@
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-};
+//let info = {
+  //personagem: 'Margarida',
+  //origem: 'Pato Donald',
+  //nota: 'Namorada do personagem principal nos quadrinhos do Pato //Donald',
+//};
 
 //console.log("Bem-vinda, " + info.personagem);
 
-info["recorrente"] = "sim";
+//info["recorrente"] = "sim";
 
 //console.log(info);
 
@@ -14,21 +14,33 @@ info["recorrente"] = "sim";
   //console.log(info[key]);
 //}
 
-let info2 = {
-  personagem: "Tio Patinhas",
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: "O Último MacPatinhas",
-  recorrente: "sim"
+//let info2 = {
+  //personagem: "Tio Patinhas",
+  //origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+ //nota: "O Último MacPatinhas",
+  //recorrente: "sim"
+//}
+
+//for(let properties in info){
+  //if (
+    //properties === 'recorrente' &&
+    //info[properties] === 'Sim' &&
+   // info2[properties] === 'Sim'
+  //) {
+  //  console.log('Ambos recorrentes');
+ // } else {
+  //  console.log(info[properties] + ' e ' + info2[properties]);
+  //}
+//}
+
+function verificaPalindrome(word){
+  for(index in word){
+    if(word[index] != word[(word.length - 1) - index]){
+      return false;
+    }
+  }
+  return true;
 }
 
-for(let properties in info){
-  if (
-    properties === 'recorrente' &&
-    info[properties] === 'Sim' &&
-    info2[properties] === 'Sim'
-  ) {
-    console.log('Ambos recorrentes');
-  } else {
-    console.log(info[properties] + ' e ' + info2[properties]);
-  }
-}
+console.log(verificaPalindrome('arara'));
+
