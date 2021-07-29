419cc4d56fee9621a5bd07a94cbdc2f5a73f1fe0
Helena <nenafgreco@hotmail.com>
2021-07-29 19:03:21 -0300

    Modificando o editor de texto

--- bloco_8/dia-1/script.js
+++ bloco_8/dia-1/script.js
@@ -30,5 +30,5 @@ const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 
 const checkAnswers = (gabarito, students) => {
-  
+
 }
