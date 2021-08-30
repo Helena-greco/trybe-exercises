7c2ae2090a3deeff8626ec5d0959e781caefa6ad
Helena <nenafgreco@hotmail.com>
2021-08-30 14:03:16 -0300

    Conteudo do dia 1

---
+++ bloco_11/dia-1/test-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Paragraph from './Component';
+
+function App() {
+  return <Paragraph />;
+}
+
+export default App;
---
+++ bloco_11/dia-1/test-app/src/Component.js
@@ -0,0 +1,11 @@
+import React,  { Component } from 'react'
+
+class Paragraph extends Component {
+  render() {
+    return <div className='App'>
+    <p>Helena Greco</p>
+  </div>
+  }
+};
+
+export default Paragraph;
