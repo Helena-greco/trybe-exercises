7ef94b166756df55b38e5fb99ad7187546e458d5
Helena <nenafgreco@hotmail.com>
2021-08-31 11:56:53 -0300

    Exercicio de fixaçao sobre props

--- bloco_11/dia-1/exercises/src/App.js
+++ bloco_11/dia-1/exercises/src/App.js
@@ -5,8 +5,8 @@ import Image from './Image';
 function App() {
   return (
     <div>
+      <Image source="https://www.homemmaquina.com.br/wp-content/uploads/2021/01/3235815-1536x1024.jpg" alternativeText="Pessoa sentada no calendário" height="400px" width="500px" />
       <TaskList />
-      <Image />
     </div>
   );
 }
--- bloco_11/dia-1/exercises/src/Image.js
+++ bloco_11/dia-1/exercises/src/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Image extends React.Component {
   render() {
-    return <img src={this.props.source} alt={this.props.alternativeText} />;
+    return <img src={this.props.source} alt={this.props.alternativeText} height={this.props.height} width={this.props.width} />;
   }
 }
 
